41ed20282a21ffe88d1f3c7bbc0f620fa7be6019
jackson cafazzo and matt knutson <mknut001@gmail.com>
2016-03-16 13:49:52 -0700

    add a marker and it will bounce

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,15 +1,74 @@
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+var map;
+var marker;
+
+function initMap() {
+  var myLatLng = {lat: 45.397, lng: -122.25}
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 10
+  });
+
+  marker = new google.maps.Marker({
+    position: myLatLng,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    map: map,
+    title: 'MARKER!!'
+  });
+  marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function locateUser() {
+  if (navigator.geolocation) {
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
+
+function geolocationSuccess(position) {
+  // here we take the `position` object returned by the geolocation api
+  // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
+  // it 2 arguments: one for latitude, one for longitude.
+  // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
+  // But you must use coordinates to use this method.
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var myOptions = {
+    zoom : 16,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map"),mapProp);
+  // Draw the map - you have to use 'getElementById' here.
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  // Place the marker
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+function geolocationError(positionError) {
+  alert(positionError);
 }
 
 
-$( document ).ready(function() {
 
+$(document).ready(function() {
+  $('#locateUser').click(locateUser);
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initMap);
 });
